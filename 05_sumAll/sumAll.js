7ff4efdc93dbd9635d97b325d56e4798d8130dd3
June <yoo.junehyuk@gmail.com>
2025-02-17 09:54:00 -0600

    Completed exercises 5-7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    let sum = 0;
+    if (start < 0 || end < 0) return "ERROR";
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+    while (start <= end) {
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
