0292a641436f7124280b5f6779fb9b15bb4597d0
June <yoo.junehyuk@gmail.com>
2025-02-26 12:38:08 -0600

    Completed all javascript exercises.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    if (person.yearOfDeath == undefined) {
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+};
 
+const findTheOldest = function(people) {
+    const years = people.sort((a, b) => {
+            const aAge = getAge(a);
+            const bAge = getAge(b);
+            return aAge < bAge ? 1 : -1;
+        });
+    console.table(years);
+    return years[0];
 };
 
 // Do not edit below this line
