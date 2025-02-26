75cda4243b2e4dc807c7c8a132f623db6c8e9665
June <yoo.junehyuk@gmail.com>
2025-02-26 12:21:09 -0600

    Completed 09_palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const isalphanumeric = function (char) {
+    const alphnum = "abcdefghijklmnopqrstuvwxyz1234567890";
+    return alphnum.includes(char);
+}
 
+
+const palindromes = function (string) {
+    const processedString = string
+                                .toLowerCase()
+                                .split("")
+                                .filter(isalphanumeric)
+                                .join("");
+
+    let a = 0;
+    let b = processedString.length-1;
+    while (a <= b) {
+        if (processedString[a] != processedString[b]) return false;
+        a++;
+        b--;
+    }
+    //console.table(processedString);
+    return true;
 };
 
 // Do not edit below this line
